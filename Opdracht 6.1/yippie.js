c4cc4d88dded315aa85de7ad68db81a8d892b4e9
Wumpus <michaeldavelaaryoshi@gmail.com>
2023-10-11 13:05:59 +0200

    e

--- Opdracht 6.1/yippie.js
+++ Opdracht 6.1/yippie.js
@@ -1,4 +1,5 @@
 function kleuraanpassen() {
     document.getElementById("h1_opdracht").style.color = "#ff0000";
-    document.getElementById("p_opdracht").innerHTML = "Hey nu is de text rood!";
+    document.getElementById("p_opdracht").innerHTML = "Hey nu is ie rood!";
    }
+   kleuraanpassen();
