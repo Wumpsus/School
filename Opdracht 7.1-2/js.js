1237d13d3bcb3fb9e2e464a7d24b8eefafc6dd4e
Wumpus <michaeldavelaaryoshi@gmail.com>
2023-10-24 10:42:44 +0200

    e

--- Opdracht 7.1-2/js.js
+++ Opdracht 7.1-2/js.js
@@ -1,18 +1,27 @@
 function controleerBudget() {
-    var invoerBedrag = prompt("Voer het bedrag in:");
-    var budget = 100;
-    var product = 60;
+    var budget = parseFloat(prompt("Wat is je budget?"));
+    var productPrijs = parseFloat(prompt("Wat is de prijs van het product dat je wilt kopen?"));
     var resultaatElement = document.getElementById("resultaat");
+    var GenoegGeldAfbeeldingElement = document.getElementById("GenoegGeldAfbeelding");
+    var ongeldigeInvoerAfbeelding = document.getElementById("ongeldigeInvoerAfbeelding");
+    var teWeinigGeldAfbeelding = document.getElementById("teWeinigGeldAfbeelding");
 
-    if (invoerBedrag === null || invoerBedrag === "" || isNaN(invoerBedrag)) {
-        resultaatElement.innerHTML = '<p style="color: black;"><strong>Ongeldige invoer. Voer een geldig bedrag in.</strong></p>';
+    if (isNaN(budget) || isNaN(productPrijs)) {
+        resultaatElement.innerHTML = '<p style="color: black;"><strong>Ongeldige invoer. Voer geldige bedragen in.</strong></p>';
+        GenoegGeldAfbeeldingElement.style.display = "none";
+        ongeldigeInvoerAfbeelding.style.display = "block";
+        teWeinigGeldAfbeelding.style.display = "none";
     } else {
-        var ingevoerdBedrag = parseFloat(invoerBedrag);
-
-        if (ingevoerdBedrag >= product) {
+        if (budget >= productPrijs) {
             resultaatElement.innerHTML = '<p style="color: green;"><strong>U heeft genoeg geld!</strong></p>';
+            GenoegGeldAfbeeldingElement.style.display = "block";
+            ongeldigeInvoerAfbeelding.style.display = "none";
+            teWeinigGeldAfbeelding.style.display = "none";
         } else {
             resultaatElement.innerHTML = '<p style="color: red;"><strong>Te weinig geld!.</strong></p>';
+            GenoegGeldAfbeeldingElement.style.display = "none";
+            ongeldigeInvoerAfbeelding.style.display = "none";
+            teWeinigGeldAfbeelding.style.display = "block";
         }
     }
 }
