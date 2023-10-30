7f938455cd3a783736a494c1f8abcf3ad9cca825
Wumpus <michaeldavelaaryoshi@gmail.com>
2023-10-30 03:59:20 +0100

    Black people

---
+++ Test/test.js
@@ -0,0 +1,4 @@
+// JavaScript code to redirect to YouTube when the button is clicked
+document.getElementById("youtube-button").addEventListener("click", function() {
+    window.location.href = "https://iku.wtf/";
+});
