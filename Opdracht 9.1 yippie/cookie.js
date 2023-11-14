4ad8ab0fd5fa67e63ef3880060ca9e23a29a5e55
Wumpus <michaeldavelaaryoshi@gmail.com>
2023-11-14 13:41:05 +0100

    e

---
+++ Opdracht 9.1 yippie/cookie.js
@@ -0,0 +1,7 @@
+function acceptCookies() {
+    var cookieWall = document.getElementById('cookieWall');
+    var content = document.getElementById('content');
+
+    cookieWall.style.display = 'none';
+    content.classList.remove('hidden');
+}
